22cfd84ab8627f0168e31eadd6d0bbcb1e40d324
Jon Jagger <jon@jaggersoft.com>
2022-06-26 08:45:17 +0100

    Upgrade to v11.2.1

--- check_version.sh
+++ check_version.sh
@@ -6,7 +6,7 @@ readonly JSON=`cat docker/image_name.json`
 readonly IMAGE_NAME="${BASH_REMATCH[1]}"
 
 readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
-readonly EXPECTED=10.2.0
+readonly EXPECTED=11.2.1
 readonly ACTUAL=$(docker run --rm -it ${IMAGE_NAME} sh -c 'gcc --version')
 
 if echo "${ACTUAL}" | grep -q "${EXPECTED}"; then
